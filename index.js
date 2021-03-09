77f1f642d5fd38d01ff8ee2254df9b3708624266
decisive-metric-0348 <gmd298@gmail.com>
2021-03-09 17:35:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // write your code below!
+
+var General = 'Happy holidays!'
+
+function happyHolidays(A) {
+  return "Happy holidays!"
+}
+
+function happyHolidaysTo(B) {
+  return "Happy holidays, ${name}!"
+}
+
+
+
+
+// function happyHolidays() {
+//   return 'Happy holidays!'
+// }
+
+// function happyHolidaysTo(name) {
+//   return `Happy holidays, ${name}!`
+// }
+
+// function happyCustomHolidayTo(holidayAndName) {
+//   return `
+// }
